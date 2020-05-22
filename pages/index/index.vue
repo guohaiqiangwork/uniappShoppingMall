0b896caf64d8266a93ae153949e85aacc49b3768
guohaiqiang <837557495@qq.com>
2020-05-22 11:34:20 +0800

    新建项目

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<cu-custom bgColor="bg-gradual-blue" :isBack="false">
+			<block slot="backText">返回</block>
+			<block slot="content">登录</block>
+		</cu-custom>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: ''
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style>
+.content {
+	display: flex;
+	flex-direction: column;
+	/* align-items: center; */
+	justify-content: center;
+}
+</style>
